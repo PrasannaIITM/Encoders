6f229d1d317d1641abb247872f8bf606fc79898c
prasanna <prasanna.bartakke@gmail.com>
2021-06-19 15:36:02 +0530

    added decode

--- arithmetic_encoding.py
+++ arithmetic_encoding.py
@@ -6,6 +6,8 @@ class ArithmeticEncoder:
     def __init__(self, freq_table, save=False):
         self.history = []
         self.history_binary = []
+        self.decoder_history = []
+
         self.save = save
         if save:
             print(
@@ -121,3 +123,31 @@ class ArithmeticEncoder:
             self.history_binary.append(stage_probabs)
 
         return binary_code
+
+    def decode(self, encoded_msg, msg_length):
+
+        decoded_msg = []
+
+        curr_min = Decimal(0.0)
+        curr_max = Decimal(1.0)
+
+        for pos in range(msg_length):
+            stage_probabs = self.evaluate_stage(curr_min, curr_max)
+
+            for key, value in stage_probabs.items():
+                if encoded_msg >= value[0] and encoded_msg <= value[1]:
+                    break
+
+            decoded_msg.append(key)
+
+            curr_min = stage_probabs[key][0]
+            curr_max = stage_probabs[key][1]
+
+            if self.save:
+                self.decoder_history.append(stage_probabs)
+
+        if self.save:
+            last_stage_probabs = self.evaluate_stage(curr_min, curr_max)
+            self.decoder_history.append(last_stage_probabs)
+
+        return "".join(decoded_msg)
